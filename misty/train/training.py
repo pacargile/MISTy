64479214868d44120cbecb904d920ce282f0009a
Phillip Cargile <pcargile@Phillips-MacBook-Pro-M1.local>
2022-06-16 14:13:22 -0400

    changed reporting freq

--- misty/train/training.py
+++ misty/train/training.py
@@ -452,7 +452,7 @@ class TrainMod(object):
                         Y_pred_valid_Tensor = model(X_valid_Tensor[idx])                        
                         loss_valid += loss_fn(Y_pred_valid_Tensor, Y_valid_Tensor[idx])
 
-                        if (iter_i % 1000 == 0) and (iter_i != 0):
+                        if (iter_i % 1000 == 0) and (iter_i != 0) and (j == 0):
                             print('--> Testing the model @ {}:'.format(iter_i))
                             print('      Input Labels:')
                             print(X_valid_Tensor[idx][:3])
